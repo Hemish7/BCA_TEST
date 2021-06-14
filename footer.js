716f878acb76b5115bc70329f7e8788248a397e8
Hemish7 <hemishpanchal7@gmail.com>
2021-06-14 19:26:37 +0530

    testing

--- footer.js
+++ footer.js
@@ -1,247 +1,93 @@
 function Hello() {
 
-  return React.createElement(
-    "div",
-    null,
-    React.createElement(
-      "footer",
-      { id: "footer" },
-      React.createElement(
-        "div",
-        { "class": "footer-top" },
-        React.createElement(
-          "div",
-          { "class": "container" },
-          React.createElement(
-            "div",
-            { "class": "row" },
-            React.createElement(
-              "div",
-              { "class": "col-lg-3 col-md-6 footer-contact" },
-              React.createElement(
-                "h3",
-                null,
-                "Flexor"
-              ),
-              React.createElement(
-                "p",
-                null,
-                "A108 Adam Street ",
-                React.createElement("br", null),
-                "New York, NY 535022",
-                React.createElement("br", null),
-                "United States ",
-                React.createElement("br", null),
-                React.createElement("br", null),
-                React.createElement(
-                  "strong",
-                  null,
-                  "Phone:"
-                ),
-                " +1 5589 55488 55",
-                React.createElement("br", null),
-                React.createElement(
-                  "strong",
-                  null,
-                  "Email:"
-                ),
-                " info@example.com",
-                React.createElement("br", null)
-              )
-            ),
-            React.createElement(
-              "div",
-              { "class": "col-lg-2 col-md-6 footer-links" },
-              React.createElement(
-                "h4",
-                null,
-                "Useful Links"
-              ),
-              React.createElement(
-                "ul",
-                null,
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Home"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "About us"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Services"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Terms of service"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Privacy policy"
-                  )
-                )
-              )
-            ),
-            React.createElement(
-              "div",
-              { "class": "col-lg-3 col-md-6 footer-links" },
-              React.createElement(
-                "h4",
-                null,
-                "Our Services"
-              ),
-              React.createElement(
-                "ul",
-                null,
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Web Design"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Web Development"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Product Management"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Marketing"
-                  )
-                ),
-                React.createElement(
-                  "li",
-                  null,
-                  React.createElement("i", { "class": "bx bx-chevron-right" }),
-                  " ",
-                  React.createElement(
-                    "a",
-                    { href: "#" },
-                    "Graphic Design"
-                  )
-                )
-              )
-            )
-          )
-        )
-      ),
-      React.createElement(
-        "div",
-        { "class": "container d-lg-flex py-4" },
-        React.createElement(
-          "div",
-          { "class": "me-lg-auto text-center text-lg-start" },
-          React.createElement(
-            "div",
-            { "class": "copyright" },
-            "\xA9 Copyright ",
-            React.createElement(
-              "strong",
-              null,
-              React.createElement(
-                "span",
-                null,
-                "Flexor"
-              )
-            ),
-            ". All Rights Reserved"
-          )
-        ),
-        React.createElement(
-          "div",
-          { "class": "social-links text-center text-lg-right pt-3 pt-lg-0" },
-          React.createElement(
-            "a",
-            { href: "#", "class": "twitter" },
-            React.createElement("i", { "class": "bx bxl-twitter" })
-          ),
-          React.createElement(
-            "a",
-            { href: "#", "class": "facebook" },
-            React.createElement("i", { "class": "bx bxl-facebook" })
-          ),
-          React.createElement(
-            "a",
-            { href: "#", "class": "instagram" },
-            React.createElement("i", { "class": "bx bxl-instagram" })
-          ),
-          React.createElement(
-            "a",
-            { href: "#", "class": "google-plus" },
-            React.createElement("i", { "class": "bx bxl-skype" })
-          ),
-          React.createElement(
-            "a",
-            { href: "#", "class": "linkedin" },
-            React.createElement("i", { "class": "bx bxl-linkedin" })
-          )
-        )
-      )
+    
+
+    return (
+    <div>
+       
+       <footer id="footer">
+
+<div class="footer-top">
+  <div class="container">
+    <div class="row">
+
+    <div class="col-lg-3 col-md-6 footer-contact">
+        <h3>Flexor</h3>
+        <p>
+          A108 Adam Street <br/>
+          New York, NY 535022<br/>
+          United States <br/><br/>
+          <strong>Phone:</strong> +1 5589 55488 55<br/>
+          <strong>Email:</strong> info@example.com<br/>
+        </p>
+    </div>
+
+      <div class="col-lg-2 col-md-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-links">
+        <h4>Our Services</h4>
+        <ul>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
+          <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
+        </ul>
+      </div>
+
+     
+
+    </div>
+  </div>
+</div>
+
+<div class="container d-lg-flex py-4">
+
+  <div class="me-lg-auto text-center text-lg-start">
+    <div class="copyright">
+      &copy; Copyright <strong><span>Flexor</span></strong>. All Rights Reserved
+    </div>
+   
+  </div>
+  <div class="social-links text-center text-lg-right pt-3 pt-lg-0">
+    <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+  </div>
+</div>
+</footer>
+
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+        </div>
     )
-  );
 }
 
-ReactDOM.render(React.createElement(Hello, null), document.getElementById('foot'));
+
+
+  ReactDOM.render(<Hello/>, document.getElementById('foot'));
--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 
 function Test() {
-  var _React$useState = React.useState(false),
-      _React$useState2 = _slicedToArray(_React$useState, 2),
-      mobileNav = _React$useState2[0],
-      setMobileNav = _React$useState2[1];
-
-  var _React$useState3 = React.useState('navbar'),
-      _React$useState4 = _slicedToArray(_React$useState3, 2),
-      navClass = _React$useState4[0],
-      setNavClass = _React$useState4[1];
-
-  var _React$useState5 = React.useState('bi bi-list mobile-nav-toggle'),
-      _React$useState6 = _slicedToArray(_React$useState5, 2),
-      menuClass = _React$useState6[0],
-      setMenuClass = _React$useState6[1];
+  
+  const [mobileNav, setMobileNav]=React.useState(false);
+  const [navClass, setNavClass] = React.useState('navbar');
+  const [menuClass, setMenuClass] = React.useState('bi bi-list mobile-nav-toggle');
   // const [activeClass, setActiveClass] = React.useState('home');
   //bi mobile-nav-toggle bi-x
-
-
-  function toggleMobileNav() {
-    if (mobileNav) {
+  
+ 
+  function toggleMobileNav () {
+    if(mobileNav) {
       setNavClass('navbar');
       setMobileNav(false);
       setMenuClass('bi bi-list mobile-nav-toggle');
@@ -31,152 +21,70 @@ function Test() {
     }
   }
 
-  return React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'section',
-      { id: 'topbar', 'class': 'd-flex align-items-center' },
-      React.createElement(
-        'div',
-        { 'class': 'container d-flex justify-content-center justify-content-md-between' },
-        React.createElement(
-          'div',
-          { 'class': 'contact-info d-flex align-items-center' },
-          React.createElement(
-            'i',
-            { 'class': 'bi bi-envelope d-flex align-items-center' },
-            React.createElement(
-              'a',
-              { href: 'mailto:contact@example.com' },
-              'contact@example.com'
-            )
-          ),
-          React.createElement(
-            'i',
-            { 'class': 'bi bi-phone d-flex align-items-center ms-4' },
-            React.createElement(
-              'span',
-              null,
-              '+1 5589 55488 55'
-            )
-          )
-        ),
-        React.createElement(
-          'div',
-          { 'class': 'cta d-none d-md-flex align-items-center' },
-          React.createElement(
-            'a',
-            { href: '#about', 'class': 'scrollto' },
-            'Get Started'
-          )
-        )
-      )
-    ),
-    React.createElement(
-      'header',
-      { id: 'header', 'class': 'd-flex align-items-center' },
-      React.createElement(
-        'div',
-        { 'class': 'container d-flex align-items-center justify-content-between' },
-        React.createElement(
-          'div',
-          { 'class': 'logo' },
-          React.createElement(
-            'h1',
-            null,
-            React.createElement(
-              'a',
-              { href: 'index.html' },
-              'Flexor'
-            )
-          )
-        ),
-        React.createElement(
-          'nav',
-          { id: 'navbar', className: navClass },
-          React.createElement(
-            'ul',
-            null,
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { href: 'index.html' },
-                'Home'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { href: 'about.html' },
-                'About'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { href: 'blog.html' },
-                'Services'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { 'class': 'nav-link scrollto ', href: '#portfolio' },
-                'Portfolio'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { 'class': 'nav-link scrollto', href: '#team' },
-                'Team'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { 'class': 'nav-link scrollto', href: '#pricing' },
-                'Pricing'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { href: 'blog.html' },
-                'Blog'
-              )
-            ),
-            React.createElement(
-              'li',
-              null,
-              React.createElement(
-                'a',
-                { 'class': 'nav-link scrollto', href: '#contact' },
-                'Contact'
-              )
-            )
-          ),
-          React.createElement('i', { onClick: toggleMobileNav, 'class': menuClass })
-        )
-      )
-    )
-  );
+  return(
+    <div>
+       
+       <section id="topbar" class="d-flex align-items-center">
+      <div class="container d-flex justify-content-center justify-content-md-between">
+      <div class="contact-info d-flex align-items-center">
+        <i class="bi bi-envelope d-flex align-items-center"><a href="mailto:contact@example.com">contact@example.com</a></i>
+        <i class="bi bi-phone d-flex align-items-center ms-4"><span>+1 5589 55488 55</span></i>
+      </div>
+
+      <div class="cta d-none d-md-flex align-items-center">
+        <a href="#about" class="scrollto">Get Started</a>
+      </div>
+    </div>
+  </section>
+  
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1><a href="index.html">Flexor</a></h1>
+        
+      </div>
+
+
+      <nav id="navbar" className={navClass}>
+        <ul>
+          <li><a  href="index.html">Home</a></li>
+          <li><a  href="about.html">About</a></li>
+          <li ><a href="blog.html">Services</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          {/* <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> */}
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i onClick={toggleMobileNav} class={menuClass}></i>
+      </nav>
+
+    </div>
+  </header>
+  
+ </div>
+  
+)
 }
 
-ReactDOM.render(React.createElement(Test, null), document.getElementById('root'));
+
+ReactDOM.render(<Test/>, document.getElementById('root'));
 // onClick={()=>setActiveClass('home')} class={activeClass === 'home'?'active':'nav-link scrollto'}
