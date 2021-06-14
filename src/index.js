5c15322333a96531c786a7d5296f0822f219bd12
Hemish7 <hemishpanchal7@gmail.com>
2021-06-14 19:22:07 +0530

    navbar

--- src/index.js
+++ src/index.js
@@ -51,11 +51,27 @@ function Test() {
           <li><a  href="index.html">Home</a></li>
           <li><a  href="about.html">About</a></li>
           <li ><a href="blog.html">Services</a></li>
-          <li><a  href="#portfolio">Portfolio</a></li>
-          <li><a href="#team">Team</a></li>
-          <li><a  href="#pricing">Pricing</a></li>
+          <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link scrollto" href="#team">Team</a></li>
+          <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
           <li><a href="blog.html">Blog</a></li>
-          
+          {/* <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">Drop Down 1</a></li>
+              <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Drop Down 2</a></li>
+              <li><a href="#">Drop Down 3</a></li>
+              <li><a href="#">Drop Down 4</a></li>
+            </ul>
+          </li> */}
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
         <i onClick={toggleMobileNav} class={menuClass}></i>
